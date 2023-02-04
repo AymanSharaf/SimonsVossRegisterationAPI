b10bc00f8d81f3a73c3528b506e85f3f1f84d50a
AymanAbdelaal <ayman.s.ismaeel@gmail.com>
2023-02-04 21:41:38 +0100

    Added ignored PackageTest.cs

---
+++ aspnet-core/test/SimmonsVoss.RegistrationApp.Domain.Tests/Packages/PackageTests.cs
@@ -0,0 +1,57 @@
+﻿using Shouldly;
+using SimmonsVoss.RegistrationApp.Features;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace SimmonsVoss.RegistrationApp.Packages
+{
+    public class PackageTests
+    {
+        public PackageTests()
+        {
+
+        }
+
+        [Fact]
+        public void Should_Create_Package_Successfully()
+        {
+            var packageName = "Package Name";
+            List<Feature> features = new List<Feature>() { Feature.CreateNew("FristFeature") };
+
+            var package = Package.CreateNew(packageName, features);
+
+            package.ShouldNotBeNull();
+            package.Name.ShouldNotBeNull().Value.ShouldNotBeNullOrEmpty();
+            package.Id.ShouldNotBeNull();
+            package.Features.ShouldNotBeNull();
+        }
+
+        [Fact]
+        public void Should_Fail_To_Create_Package_If_Name_Is_Empty()
+        {
+            var packageName = "";
+            List<Feature> features = new List<Feature>() { Feature.CreateNew("FristFeature") };
+
+            Should.Throw<ArgumentException>(() =>
+            {
+                Package.CreateNew(packageName, features);
+            });
+
+        }
+
+
+        [Fact]
+        public void Should_Add_Feature_To_Package_Successfully()
+        {
+            var packageName = "Package Name";
+            List<Feature> features = new List<Feature>() { Feature.CreateNew("FristFeature") };
+            var package = Package.CreateNew(packageName, features);
+            var newFeature = Feature.CreateNew("NewFeature");
+
+            package.AddFeature(newFeature);
+
+            Assert.Contains(package.Features, feature => feature.Equals(feature));
+        }
+    }
+}
